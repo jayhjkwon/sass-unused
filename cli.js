a8a6a692488fb6299c97428bc56d935dcf738cca
Jay Kwon <jay.kwon@rea-group.com>
2021-07-27 18:28:22 +1000

    Show used variables and mixins

--- cli.js
+++ cli.js
@@ -9,14 +9,16 @@ process.argv.slice(2).forEach(arg => {
   srcFiles = srcFiles.concat(glob.sync(arg));
 });
 
-function printUnusedList(type, idents) {
-  idents.sort().forEach(ident => console.log(`unused ${type}: ${ident.path} - ${ident.id}`));
+function print(type, idents) {
+  idents.sort().forEach(ident => console.log(`${type}: ${ident.path} - ${ident.id}`));
 }
 
-const { vars, mixins, functions } = findUnused(srcFiles);
-printUnusedList('variable', vars);
-printUnusedList('mixin', mixins);
-printUnusedList('functions', functions);
+const { vars, mixins, functions, usedVars, usedMixins } = findUnused(srcFiles);
+print('unused variable', vars);
+print('unused mixin', mixins);
+print('unused functions', functions);
+print('used varianble', usedVars);
+print('used Mixins', usedMixins);
 
 if (vars.length || mixins.length || functions.length) {
   process.exit(1);
